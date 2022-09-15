16dbbf0a1a5d50e55913da7a65d2e75093cab04d
Dhiwa Arya Kusumah <dhiwaarya@Dhiwas-MacBook-Pro.local>
2022-09-15 11:36:38 +0700

    Lab 2 changes

---
+++ wishlist/views.py
@@ -0,0 +1,33 @@
+from ast import Param
+from django.http import HttpResponse
+from django.core import serializers
+from django.shortcuts import render
+from wishlist.models import ItemWishlist
+
+# Create your views here.
+
+def show_wishlist(request):
+    data_wishlist_item = ItemWishlist.objects.all()
+    context = {
+        'list_item': data_wishlist_item,
+        'name': 'Dhiwa Arya Kusumah'
+    }
+    return render(request, "wishlist.html", context)
+
+
+def show_xml(request):
+    data = ItemWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = ItemWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = ItemWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = ItemWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
